c891b9d45fa1880fb2c1210b4b324066b7070eb4
unknown <mailtovimal87@gmail.com>
2020-03-07 22:01:37 +0530

    Autowiring

--- src/main/java/com/demo/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/demo/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package com.demo.moviecatalogservice.resources;
 import com.demo.moviecatalogservice.models.CatalogItem;
 import com.demo.moviecatalogservice.models.Movie;
 import com.demo.moviecatalogservice.models.MovieRating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +12,14 @@ import org.springframework.web.client.RestTemplate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
@@ -23,20 +27,28 @@ public class MovieCatalogResource {
         // for all the rated movies call the movie-detail-service using movieId from rating list
         //collect all the movies and return
 
+
+
         List<MovieRating> ratings = Arrays.asList(
           new MovieRating("12345", 7),
           new MovieRating("123456", 8)
         );
 
-        // response list
-        List<CatalogItem> responseList = new ArrayList<>();
-        for(MovieRating movieRating : ratings){
-            RestTemplate restTemplate = new RestTemplate();
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movie/" + movieRating.getMoveId(), Movie.class);
-            CatalogItem catalogItem = new CatalogItem(movie.getMovieId(), movie.getMovieName(), movie.getDesc(), movieRating.getRating());
-            responseList.add(catalogItem);
+        return ratings.stream().map(movieRating -> {
+                    Movie movie = restTemplate.getForObject("http://localhost:8082/movie/" + movieRating.getMoveId(), Movie.class);
+                    return new CatalogItem(movie.getMovieId(), movie.getMovieName(), movie.getDesc(), movieRating.getRating());
 
-        }
-        return  responseList;
+                }
+                ).collect(Collectors.toList());
+        // response list
+//        List<CatalogItem> responseList = new ArrayList<>();
+//        for(MovieRating movieRating : ratings){
+//            RestTemplate restTemplate = new RestTemplate();
+//            Movie movie = restTemplate.getForObject("http://localhost:8082/movie/" + movieRating.getMoveId(), Movie.class);
+//            CatalogItem catalogItem = new CatalogItem(movie.getMovieId(), movie.getMovieName(), movie.getDesc(), movieRating.getRating());
+//            responseList.add(catalogItem);
+//
+//        }
+
     }
 }
